2f470fbcf9501d215af8cea7028f0cc37f9eb323
pixel0verflow <pixel0verflow@icloud.com>
2026-05-07 21:00:48 +0200

    feat: scaffold sb app

---
+++ src/main/java/net/bartcloud/finbrake/FinbrakeApplication.java
@@ -0,0 +1,13 @@
+package net.bartcloud.finbrake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbrakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinbrakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bartcloud/finbrake/FinbrakeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bartcloud.finbrake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinbrakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
